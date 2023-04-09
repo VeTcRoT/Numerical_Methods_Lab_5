cecbb484aaa09d1c341e491b3d65e50965f1e422
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-04-09 21:36:44 +0300

    Implemented Interpolation Polynomial

---
+++ Lab_5/Lab_5/InterpolationPolynomial.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    internal class InterpolationPolynomial
+    {
+        public double[] XCoordinates { get; set; }
+        public double[] YCoordinates { get; set; }
+        public double X0 { get; set; }
+        public int N { get => XCoordinates.Length; }
+        public InterpolationPolynomial(double[] xCoordinates, double[] yCoordinates, double x0)
+        {
+            XCoordinates = xCoordinates;
+            YCoordinates = yCoordinates;
+            X0 = x0;
+        }
+        private double[,] GetMatrixCoefficients()
+        {
+            var coefficients = new double[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    coefficients[i, j] = Math.Pow(XCoordinates[i], j);
+                }
+            }
+            return coefficients;
+        }
+        private double[] SolveSystem()
+        {
+            double[,] coefficients = GetMatrixCoefficients();
+            double[] valuesVector = (double[])YCoordinates.Clone();
+
+            for (int k = 0; k < N - 1; k++)
+            {
+                for (int i = k + 1; i < N; i++)
+                {
+                    double factor = coefficients[i, k] / coefficients[k, k];
+                    for (int j = k + 1; j < N; j++)
+                    {
+                        coefficients[i, j] -= factor * coefficients[k, j];
+                    }
+                    valuesVector[i] -= factor * valuesVector[k];
+                }
+            }
+            double[] x = new double[N];
+
+            x[N - 1] = valuesVector[N - 1] / coefficients[N - 1, N - 1];
+
+            for (int i = N - 2; i >= 0; i--)
+            {
+                double sum = 0;
+                for (int j = i + 1; j < N; j++)
+                {
+                    sum += coefficients[i, j] * x[j];
+                }
+                x[i] = (valuesVector[i] - sum) / coefficients[i, i];
+            }
+
+            return x;
+        }
+        public double GetResult()
+        {
+            double[] c = SolveSystem();
+
+            double y0 = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                y0 += c[i] * Math.Pow(X0, i);
+            }
+
+            return y0;
+        }
+    }
+}
---
+++ Lab_5/Lab_5/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lab_5;
+
+double[] x = { 1, 2, 3, 4 };
+double[] y = { -3, -1, 0, 7 };
+
+double x0 = 1.75;
+
+var interpolationPolynomial = new InterpolationPolynomial(x, y, x0);
+
+Console.WriteLine(new string('-', 30) + "Task 1" + new string('-', 30));
+Console.WriteLine("\ninterpolation Polynomial: " + interpolationPolynomial.GetResult());
